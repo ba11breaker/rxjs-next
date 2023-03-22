666a5b2bcbfcd7ec21361bdc58e797af8ca91ab4
ba11breaker <15212219417@163.com>
2023-03-23 00:25:07 +1000

    feat: created Observable enter$

--- components/SmartCounter/index.tsx
+++ components/SmartCounter/index.tsx
@@ -1,18 +1,40 @@
 import React from "react";
+import { BehaviorSubject, combineLatestAll, filter, map, tap, withLatestFrom } from "rxjs";
 import "./index.css"
 
 const takeUntilFunc = (endRange: number, currentNumber: number) => {
   return endRange > currentNumber
     ? (val: number) => val <= endRange
     : (val: number) => val >= endRange
-}
+};
+
+const positiveOrNegative = (endRange: number, currentNumber: number) => {
+  return endRange > currentNumber ? 1 : -1;
+};
 
 const SmartCounter : React.FC = () => {
   const [currentNumber, setCurrentNumber] =  React.useState<number>(0);
 
+  const inputSubject = React.useMemo(() => new BehaviorSubject<string >(''), []);
+  const keyUpSubject = React.useMemo(() => new BehaviorSubject<string>(''), []);
+
+  const inputChange$ = React.useMemo(() => inputSubject, [inputSubject]);
+
+  const enter$ = React.useMemo(() => keyUpSubject.pipe(
+    filter(value => value === 'Enter'),
+    withLatestFrom(inputChange$),
+  ), [keyUpSubject, inputChange$]);
+
+  React.useEffect(() => {
+    const sub = enter$.subscribe(console.log);
+    return () => {
+      sub.unsubscribe();
+    };
+  }, [enter$]);
+
   return (
     <div className="container">
-      <input id="range" type="number" />
+      <input id="range" type="number" onChange={evt => inputSubject.next(evt.target.value)} onKeyUp={evt => keyUpSubject.next(evt.key)} />
       <h1 id="display"></h1>
     </div>
   )
